33fd6e5fe8b06145fed89699eb9e0932d17a7b40
unknown <ssing213@asu.edu>
2019-10-09 15:28:44 -0700

    Part 1: create the Initial version

---
+++ cse360assign2/AddingMachine.java
@@ -0,0 +1,59 @@
+/**
+* @author  Somesh Singh
+* @classId 85141
+* @Assignment No. 2
+* 
+* The AddingMachine.java file represents an adding machine which 
+* which when implemented fully adds, subtracts and gets total of the operands.
+* It also returns the history of the transactions(add, subtract) as applied 
+* to the operands using the toString method
+* 
+*/
+
+package cse360assign2;
+
+public class AddingMachine {
+
+	private int total;
+	
+	/**
+	 * This is a constructor of the AddingMachine.java class which 
+	 * initializes the total to 0
+	 */
+	public AddingMachine () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	/**
+	 * This method returns the total
+	 */
+	public int getTotal () {
+		return 0;
+	}
+	
+	/**
+	 * This method is responsible for the addition of the operands
+	 */
+	public void add (int value) {
+		
+	}
+	
+	/**
+	 * This method performs the subtraction 
+	 */
+	public void subtract (int value) {
+		
+	}
+	
+	/**
+	 * This method gives a history of the all the operands and operations 
+	 * performed
+	 */
+	public String toString () {
+		return "";
+	}
+
+	public void clear() {
+	
+	}
+}
